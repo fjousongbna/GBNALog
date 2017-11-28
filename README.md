ddc64d816dd51b1a6b8ffeff86f597d2e7830b99
Franck JOUSON <f.jouson@bordeauxnord.com>
2017-11-28 16:59:57 +0100

    Version complète avec tests

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # GBNALog
 
-Experimentation of log manager
+[![Build status](https://ci.appveyor.com/api/projects/status/02eby9l3jo14mud5?svg=true)](https://ci.appveyor.com/project/fjousongbna/gbnalog)
+
+Experimentation of Powershell with a log manager
 
 __TODO :__ Documentation of this project
---
+++ src/GBNALog.Tests.ps1
@@ -0,0 +1,34 @@
+﻿Import-Module -Name GBNAlog
+
+$logFile = "TestDrive:\output.log"
+
+Describe "Test module_log" {
+    Context "Tests for Header and Footer" {
+        It "Implements the header" {
+            Write-GBNALog -Filepath $logFile -Header
+            Get-Content $logFile | Select-String -Pattern '^When generated' -Quiet | Should Be $true
+        }
+        It "Implements the footer" {
+            Write-GBNALog -Filepath $logFile -Footer
+            Get-Content $logFile | Select-String -Pattern '^End time' -Quiet | Should Be $true
+        }
+    }
+    Context "Implements INFORMATION log in the body" {
+        It "Writes an INF entry" {
+            Write-GBNALog -Filepath $logFile -Category "INF" -Message "Ligne d'information"
+            Get-Content $logFile | Select-String -Pattern 'INF;' -Quiet | Should Be $true
+        }
+    }
+    Context "Implements WARNING log in the body" {
+        It "Writes a WAR entry" {
+            Write-GBNALog -Filepath $logFile -Category "WAR" -Message "Ligne de warning"
+            Get-Content $logFile | Select-String -Pattern 'WAR;' -Quiet | Should Be $true
+        }
+    }
+    Context "Implements ERROR in the body" {
+        It "Writes an ERR entry" {
+            Write-GBNALog -Filepath $logFile -Category "ERR" -Message "Ligne d'erreur"
+            Get-Content $logFile | Select-String -Pattern 'ERR;' -Quiet | Should Be $true
+        }
+    }
+}
